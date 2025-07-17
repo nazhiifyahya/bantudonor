32f1142e25014df64110d46f66a156f78860719b
nazhiifyahya <mnazhiifys@gmail.com>
2025-07-17 13:47:01 +0700

    edit folder name for layout

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $currentPage = 'home';
 $featuredRequests = [];
 
 // Include header template
-include 'includes/header.php';
+include 'layout/header.php';
 ?>
 
     <!-- Hero Section -->
@@ -67,4 +67,4 @@ include 'includes/header.php';
         </div>
     </section>
 
-<?php include 'includes/footer.php'; ?>
+<?php include 'layout/footer.php'; ?>
