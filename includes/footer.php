e6c062d47270af3c50c7371e1befa2de77192edf
nazhiifyahya <mnazhiifys@gmail.com>
2025-07-17 14:06:09 +0700

    deleting because folder name changed

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <!-- Footer -->
-    <footer class="bg-gray-900 w-full px-20 py-10 flex justify-between items-center max-w-[1280px] relative mx-auto">
-        <div class="text-slate-50 text-base font-normal">© <?php echo date('Y'); ?> BantuDonor. All rights reserved.</div>
-    </footer>
-
-</body>
-</html>
