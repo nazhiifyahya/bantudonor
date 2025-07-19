9432211113a755aef5d2279a5d19362543882157
nazhiifyahya <mnazhiifys@gmail.com>
2025-07-19 12:59:12 +0700

    adding capabilities and usage of .env file

--- config/database.php
+++ config/database.php
@@ -3,14 +3,27 @@
  * Database Configuration
  * BantuDonor Application
  */
+require_once __DIR__ . '/envloader.php';
 
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'bantudonor_db';
-    private $username = 'root';
-    private $password = '';
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     private $charset = 'utf8mb4';
     public $conn;
+
+    /**
+     * Constructor
+     * Initializes the database connection parameters
+     */
+
+    public function __construct() {
+        $this->host = $_ENV['MySQL_DB_HOST'];
+        $this->db_name = $_ENV['MySQL_DB_NAME'];
+        $this->username = $_ENV['MySQL_DB_USER'];
+        $this->password = $_ENV['MySQL_DB_PASSWORD'];
+    }
 
     /**
      * Get database connection
