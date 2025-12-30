0d0e6cd94b568e26219bf09afe066fbaa7109da8
nazhiifyahya <mnazhiifys@gmail.com>
2025-12-30 20:02:59 +0700

    changing place holder for phone number

--- register.php
+++ register.php
@@ -163,7 +163,7 @@ include 'layout/header.php';
                     <input type="tel" 
                            name="phone" 
                            required
-                           placeholder="Contoh: 6285xxxxxxxx"
+                           placeholder="Contoh: 085xxxxxxxx"
                            value="<?php echo isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : ''; ?>"
                            class="w-full px-4 py-3 rounded-lg border border-slate-300 focus:border-red-500 focus:outline-none">
                 </div>
